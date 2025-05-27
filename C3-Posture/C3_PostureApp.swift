18e26a7dd1165db5f1551aaf93299cdc625d2039
ahwan <ahmadkurniawanibrahim@gmail.com>
2025-05-27 13:48:20 +0700

    Initial Commit

---
+++ C3-Posture/C3_PostureApp.swift
@@ -0,0 +1,17 @@
+//
+//  C3_PostureApp.swift
+//  C3-Posture
+//
+//  Created by Ahmad Kurniawan Ibrahim on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct C3_PostureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
