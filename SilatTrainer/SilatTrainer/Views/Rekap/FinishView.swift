2ef0f834ad932d543bf5117cd4c3ddab968d0972
Muhammad Rifki Hidayatullah <108870501+Dylm54@users.noreply.github.com>
2025-06-24 22:13:06 +0700

    add animation in finish view

--- SilatTrainer/SilatTrainer/Views/Rekap/FinishView.swift
+++ SilatTrainer/SilatTrainer/Views/Rekap/FinishView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FinishView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var move: Bool = false
     
     var navigate: (AppRoute) -> Void
     var body: some View {
@@ -16,13 +17,28 @@ struct FinishView: View {
             Image(.finishBg)
                 .ignoresSafeArea()
             
+                Image(.sabukFinish)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding(.bottom, 200)
+                    .offset(y: -130)
+                    .scaleEffect(move ? 1.07 : 1)
+                    .animation(.linear.repeatForever(autoreverses: true).speed(0.55), value: move)
+                    .onAppear() {
+                            move = true
+                    }
+
+            
+            
             VStack() {
-                Spacer().frame(height: 450)
-                
+                Spacer().frame(height: 430)
+                    
                 Text("SELAMAT")
                     .font(.system(size: 37.5))
                     .fontWeight(.bold)
                     .foregroundStyle(.white)
+                
                 Text("Kamu Sudah Berlatih Hari Ini")
                     .font(.title2)
                     .foregroundStyle(.white)
